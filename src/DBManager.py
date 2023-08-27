e5e63728e829d34a29a5775f088753d97ca1a612
Aleksandr Ievlev <a.a.ievlev@gmail.com>
2023-08-27 05:25:05 +0300

    прописал get_all_vacancies

--- src/DBManager.py
+++ src/DBManager.py
@@ -26,7 +26,18 @@ class DBManager:
         """
         получает список всех вакансий с указанием названия компании, названия вакансии и зарплаты и ссылки на вакансию.
         """
-        pass
+        conn = psycopg2.connect(dbname=self.name_database, **self.params)
+        with conn.cursor() as cur:
+            cur.execute('SELECT companies.company_name, vacancy_name, ((salary_from + salary_to)/2) AS salary, vacancy_url '
+                        'FROM vacancies '
+                        'INNER JOIN companies '
+                        'USING (company_id) '
+                        "WHERE ((salary_from + salary_to)/2) > 0 AND salary_currency = "RUR"')
+                        'ORDER BY salary DESC')
+            rows = cur.fetchall()
+            for row in rows:
+                print(f'В компании "{row[0]}" имеется вакансия "{row[1]}".'
+                      f'Зарплата - {row[2]}. Ссылка на вакансию: {row[3]}')
 
     def get_avg_salary(self):
         """
